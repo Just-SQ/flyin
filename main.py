4f4dc71a7918ff58b6b426cbcf8da6f950d38b5e
Elbachir Saqi <elbachirsaqi@MacBook-Air-de-Elbachir.local>
2026-05-22 15:50:55 +0100

    main comment

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from simulation import Simulation
 
 
 def main():
+    # IMPORTANT -> THE Y IS FLIPED THE HUBS THAT NEED TO BE SHOWN UP IT SHOWED DOWN AND VICE VERSA
     path = "maps/challenger/01_the_impossible_dream.txt"
     p = MapParser(path)
     p.parse()
